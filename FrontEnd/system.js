24034df7a5ebd562714f0ae100c91753c70d1858
Arthuros <arthur-a54@hotmail.fr>
2023-01-26 06:52:50 +0100

    Gallery Generation upgrated + Categories buttons added

--- FrontEnd/system.js
+++ FrontEnd/system.js
@@ -1,21 +1,98 @@
 const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 
-for (let i = 0; i< works.length ; i++){
-    const gallery = document.querySelector(".gallery");
-    const work = works[i];
+
+/* GALLERY GENERATION */
+
+const gallery = document.querySelector(".gallery");
+
+async function GenerateGallery(works){
     
-    const figure = document.createElement("figure");
+    for (let i = 0; i< works.length ; i++){
     
-    const figureImg = document.createElement("img");
-    figureImg.src= work.imageUrl;
-    figureImg.alt = work.title;
+        const work = works[i];
+        const reponseImg = await fetch(work.imageUrl);
+        const images = await reponseImg.blob();
+        const imageURL = URL.createObjectURL(images);
+        
+        
     
-    const figcaption = document.createElement("figcaption");
-    figcaption.innerText = work.title;
-    
-    figure.appendChild(figureImg);
-    figure.appendChild(figcaption);
-    gallery.appendChild(figure);
+        const figure = document.createElement("figure");
+        const figureImg = document.createElement("img");
+        figureImg.src= imageURL;
+        figureImg.alt = work.title;
+        
+        const figcaption = document.createElement("figcaption");
+        figcaption.innerText = work.title;
+        
+        figure.appendChild(figureImg);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    }
 }
 
+function UpdateGallery(works){
+    gallery.innerHTML = "";
+    GenerateGallery(works);
+}
+
+
+UpdateGallery(works);
+
+
+
+/* CATEGORIES BUTTONS */
+
+const btnCategoriesBox = document.querySelector(".btnCategoriesBox");
+
+const btnAll = document.createElement("button");
+btnAll.className = "btnAll";
+btnAll.innerText = "Tous";
+
+const btnObjects = document.createElement("button"); 
+btnObjects.className = "btnObjects";
+btnObjects.innerText = "Objets";
+
+const btnApparts = document.createElement("button");
+btnApparts.className = "btnApparts";
+btnApparts.innerText = "Appartements"; 
+
+const btnHR = document.createElement("button"); 
+btnHR.className = "btnHR";
+btnHR.innerText = "Hôtels & Restaurants";
+
+
+btnCategoriesBox.appendChild(btnAll);
+btnCategoriesBox.appendChild(btnObjects);
+btnCategoriesBox.appendChild(btnApparts);
+btnCategoriesBox.appendChild(btnHR);
+
+
+    /*Buttons Functions */
+
+
+btnAll.addEventListener("click", function(){
+    UpdateGallery(works);
+    return;
+});
+
+btnObjects.addEventListener("click",function(){
+    const list = works.filter(function(work){
+        return work.categoryId === 1;
+    });
+    UpdateGallery(list);
+});
+
+btnApparts.addEventListener("click",function(){
+    const list = works.filter(function(work){
+        return work.categoryId === 2;
+    });
+    UpdateGallery(list);
+});
+
+btnHR.addEventListener("click",function(){
+    const list = works.filter(function(work){
+        return work.categoryId === 3;
+    });
+    UpdateGallery(list);
+});
